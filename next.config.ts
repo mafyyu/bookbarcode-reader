f08c5d903208a2bcae0f9ba9b987d743a84693e4
mafyyu <morita0126m@gmail.com>
2025-12-25 01:41:51 +0900

    pwaテスト

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,11 @@
 import type { NextConfig } from "next";
+import nextPWA from 'next-pwa';
+
+const withPWA = nextPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
