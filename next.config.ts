f858941cb6f440c4c44417848448f72e4c326c69
mafyyu <morita0126m@gmail.com>
2025-12-25 14:13:07 +0900

    fix/delete-nextpwa

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,4 @@
 import type { NextConfig } from "next";
-import nextPWA from 'next-pwa';
-
-const withPWA = nextPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-});
-
 const nextConfig: NextConfig = {
   /* config options here */
 };
