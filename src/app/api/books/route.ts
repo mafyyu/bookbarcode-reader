9954dcb34db38a02c650cf1b458e25d02d14252c
mafyyu <morita0126m@gmail.com>
2025-12-27 15:07:25 +0900

    feat:本の情報取得APIの作成

---
+++ src/app/api/books/route.ts
@@ -0,0 +1,52 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+export async function GET(request: NextRequest) {
+  const isbnNumber = request.nextUrl.searchParams.get("isbn");
+
+  // isbnのバーコードかどうかをチェック
+  const IsbnSchema = z
+    .string()
+    .length(13)
+    .regex(/^(978|979)\d{10}$/, "Invalid ISBN");
+
+  const ResponseSchema = z.object({
+    title: z.string(),
+    author: z.string(),
+    largeImageUrl: z.url(),
+    isbn: IsbnSchema,
+  });
+
+  const BookResponseSchema = z
+    .object({
+      Items: z.array(
+        z.object({
+          Item: ResponseSchema,
+        }),
+      ),
+    })
+    .transform((data) =>
+      data.Items.map(({ Item }) => ({
+        title: Item.title,
+        author: Item.author,
+        isbn: Item.isbn,
+        image: Item.largeImageUrl ?? null,
+      })),
+    );
+
+  const parsed = IsbnSchema.safeParse(isbnNumber);
+
+  // バーコードが適していない場合にはエラーを返す
+  if (!parsed.success) {
+    return NextResponse.json({ error: "Invalid ISBN" }, { status: 400 });
+  }
+
+  const res = await fetch(
+    `${process.env.RAKUTEN_API_BASE_URL}&isbn=${isbnNumber}`,
+  );
+  const data = await res.json();
+  const parsedResponse = BookResponseSchema.safeParse(data);
+
+  console.log(data.Items);
+  return NextResponse.json(parsedResponse);
+}
