e552bc3c823f242e5f9c4339c68a88e82232dab0
mafyyu <morita0126m@gmail.com>
2025-12-30 04:15:04 +0900

    feat:dividerの作成

---
+++ src/components/Divider.tsx
@@ -0,0 +1,26 @@
+const styles = {
+  container: {
+    display: "flex",
+    width: "80%",
+    flexDirection: "column",
+  },
+  text: {
+    paddingLeft: "10px",
+  },
+  divider: {
+    width: "100%",
+    height: "2px",
+    backgroundColor: "#E5E7EB",
+  },
+} as const;
+
+export default function Divider({ text }: { text: string }) {
+  return (
+    <>
+      <div style={styles.container}>
+        <h3 style={styles.text}>{text}</h3>
+        <div style={styles.divider}></div>
+      </div>
+    </>
+  );
+}
