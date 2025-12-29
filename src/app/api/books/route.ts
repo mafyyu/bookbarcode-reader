132a620dae19b8b2b2a6df57fa2caf57a4d5533c
mafyyu <morita0126m@gmail.com>
2025-12-30 01:14:47 +0900

    fix:apiをGETからPOSTに変更

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,61 +1,64 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
-export async function GET(request: NextRequest) {
-  const isbnNumber = request.nextUrl.searchParams.get("isbn");
+// isbnのバーコードかどうかをチェック
+const IsbnSchema = z
+  .string()
+  .length(13)
+  .regex(/^(978|979)\d{10}$/, "Invalid ISBN");
 
-  if (isbnNumber === null || isbnNumber.trim() === "") {
-    return NextResponse.json(
-      { error: 'Missing required "isbn" query parameter' },
-      { status: 400 },
-    );
+const bodySchema = z.object({
+  isbn: IsbnSchema,
+});
+
+const ResponseSchema = z.object({
+  title: z.string(),
+  titleKana: z.string(),
+  author: z.string(),
+  publisherName: z.string(),
+  largeImageUrl: z.url().optional(),
+  isbn: IsbnSchema,
+  salesDate: z.string(),
+});
+
+const BookResponseSchema = z
+  .object({
+    Items: z.array(
+      z.object({
+        Item: ResponseSchema,
+      }),
+    ),
+  })
+  .transform((data) =>
+    data.Items.map(({ Item }) => ({
+      title: Item.title,
+      titleKana: Item.titleKana,
+      author: Item.author,
+      publisherName: Item.publisherName,
+      isbn: Item.isbn,
+      image: Item.largeImageUrl ?? null,
+      salesDate: Item.salesDate,
+    })),
+  );
+
+export async function POST(request: NextRequest) {
+  let body;
+
+  try {
+    body = await request.json();
+  } catch {
+    return NextResponse.json({ error: "Invalid JSON body" }, { status: 400 });
   }
-  // isbnのバーコードかどうかをチェック
-  const IsbnSchema = z
-    .string()
-    .length(13)
-    .regex(/^(978|979)\d{10}$/, "Invalid ISBN");
-
-  const ResponseSchema = z.object({
-    title: z.string(),
-    titleKana: z.string(),
-    author: z.string(),
-    publisherName: z.string(),
-    largeImageUrl: z.url().optional(),
-    isbn: IsbnSchema,
-    salesDate: z.string(),
-  });
-
-  const BookResponseSchema = z
-    .object({
-      Items: z.array(
-        z.object({
-          Item: ResponseSchema,
-        }),
-      ),
-    })
-    .transform((data) =>
-      data.Items.map(({ Item }) => ({
-        title: Item.title,
-        titleKana: Item.titleKana,
-        author: Item.author,
-        publisherName: Item.publisherName,
-        isbn: Item.isbn,
-        image: Item.largeImageUrl ?? null,
-        salesDate: Item.salesDate,
-      })),
-    );
-
-  const parsed = IsbnSchema.safeParse(isbnNumber);
+  const parsedBody = bodySchema.safeParse(body);
 
   // バーコードが適していない場合にはエラーを返す
-  if (!parsed.success) {
+  if (!parsedBody.success) {
     return NextResponse.json({ error: "Invalid ISBN" }, { status: 400 });
   }
 
-  const res = await fetch(
-    `${process.env.RAKUTEN_API_BASE_URL}&isbn=${isbnNumber}`,
-  );
+  const { isbn } = parsedBody.data;
+
+  const res = await fetch(`${process.env.RAKUTEN_API_BASE_URL}&isbn=${isbn}`);
   if (!res.ok) {
     return NextResponse.json(
       { error: "Failed to fetch data from Rakuten API" },
