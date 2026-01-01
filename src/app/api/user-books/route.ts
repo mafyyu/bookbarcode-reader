4eea4614855f2e81ca5d8ac5b8eeb476935e54d7
mafyyu <morita0126m@gmail.com>
2026-01-01 16:56:56 +0900

    feat:ユーザのデータのみをフェッチするAPI

--- src/app/api/user-books/route.ts
+++ src/app/api/user-books/route.ts
@@ -1,48 +1,66 @@
 import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 import { IsbnSchema } from "@/lib/schema/book";
+import { BookSchema } from "@/lib/schema/book";
 import { supabase } from "@/lib/supabase";
 import z from "zod";
 
-type LibraryStatus = "not_in_library" | "in_library" | "owned";
+type Book = z.infer<typeof BookSchema>;
 
-// user_booksに存在するか確認するAPI
-export async function GET(request: NextRequest) {
+type UserBook = Book & {
+  isOwned: boolean;
+};
+
+// ユーザが登録した本を全て取得するAPI
+export async function GET() {
   const { userId } = await auth();
-  if (!userId)
+  if (!userId) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-
-  const isbn = request.nextUrl.searchParams.get("isbn");
-  if (!isbn) {
-    return NextResponse.json({ error: "ISBN required" }, { status: 400 });
   }
 
-  const parsedIsbn = IsbnSchema.safeParse(isbn);
-  if (!parsedIsbn.success) {
-    return NextResponse.json({ error: "Invalid ISBN data" }, { status: 400 });
+  try {
+    const { data, error } = await supabase
+      .from("user_books")
+      .select(
+        `
+        is_owned,
+        books (
+          isbn,
+          title,
+          titleKana,
+          author,
+          publisherName,
+          image,
+          caption,
+          salesDate
+        )
+      `,
+      )
+      .eq("user_id", userId);
+
+    if (error) {
+      console.error(error);
+      return NextResponse.json(
+        { error: "Failed to fetch user books" },
+        { status: 500 },
+      );
+    }
+
+    const result = data
+      .filter((row) => row.books !== null)
+      .map((row) => ({
+        isOwned: row.is_owned,
+        book: row.books,
+      }));
+
+    return NextResponse.json(result);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Unexpected server error" },
+      { status: 500 },
+    );
   }
-
-  const { data, error } = await supabase
-    .from("user_books")
-    .select("is_owned")
-    .eq("isbn", parsedIsbn.data)
-    .eq("user_id", userId)
-    .maybeSingle();
-
-  if (error) {
-    return NextResponse.json({ error: "DB error" }, { status: 500 });
-  }
-  let status: LibraryStatus;
-
-  if (!data) {
-    status = "not_in_library";
-  } else if (data.is_owned) {
-    status = "owned";
-  } else {
-    status = "in_library";
-  }
-
-  return NextResponse.json({ status: status });
 }
 
 const bodySchema = z.object({
