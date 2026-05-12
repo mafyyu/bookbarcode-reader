b05a14d66a5c0810298d7faffa576beaf98c5055
mafyyu <morita0126m@gmail.com>
2026-05-13 00:06:49 +0900

    fix/cashe

--- src/app/library/books/[isbn]/page.tsx
+++ src/app/library/books/[isbn]/page.tsx
@@ -20,6 +20,7 @@ export default async function BookDetails({
         headers: {
           Cookie: cookieStore.toString(),
         },
+        cache: "no-store",
       },
     );
     if (!res.ok) {
