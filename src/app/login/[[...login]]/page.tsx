476c656672524065d3578561839688e905eadb09
mafyyu <morita0126m@gmail.com>
2026-05-12 18:49:29 +0900

    fix/不要なconsoleの削除

--- src/app/login/[[...login]]/page.tsx
+++ src/app/login/[[...login]]/page.tsx
@@ -3,7 +3,6 @@ import { SignUp } from "@clerk/nextjs";
 import styles from "./page.module.css";
 
 export default function LoginPage() {
-  console.log(styles);
   return (
     <div className={styles.container}>
       <SignUp routing="hash" fallbackRedirectUrl="/library" />
